5e79c31105b897359f2a4ff44e69056abc17ee46
khanh <khanhdacodau2007@gmail.com>
2023-11-28 21:55:06 +0700

    HOP QUA TRONG HAi CON TRO

--- HaiConTro/hopqua.cpp
+++ HaiConTro/hopqua.cpp
@@ -1,52 +1,46 @@
 #include <bits/stdc++.h>
 #include <cstdio>
-using namespace  std;
+using namespace std;
 
-int giaithua(const int &n)
-{
-  int s=1;
-  for (int i=1;i<=n;i++)
-  {
-    s*=i;
+int giaithua(const int &n) {
+  int s = 1;
+  for (int i = 1; i <= n; i++) {
+    s *= i;
   }
   return s;
 }
-int tohop(const int &n)
-{
-  return giaithua(n)/(giaithua(n-3)*giaithua(3));
+int tohop(const int &n) {
+  return giaithua(n) / (giaithua(n - 3) * giaithua(3));
 }
-int main (int argc, char *argv[]) {
-  int n,k;
-  cin>>n>>k;
+int main(int argc, char *argv[]) {
+  int n, k;
+  cin >> n >> k;
   vector<int> a(n);
-  for(int i=0;i<n;i++)
-  {
-    cin>>a[i];
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
   }
-  sort(a.begin(),a.end());
-  int i=0,j=2,s=0,sdem=0;
-  for (int i:a) cout<<i<<" ";
-  cout<<'\n';
-  for(int j=2;j<n;j++)
-  {
-    if (a[j]-a[i]<=3)
-    {
-      sdem+=tohop(j-i+1);
-      //cout<<a[i]<<" "<<a[j]<<'\n';
-    }
-    else {
+  sort(a.begin(), a.end());
+  int i = 0, j = 2, s = 0, sdem = 0;
+  for (int i : a)
+    cout << i << " ";
+  cout << '\n';
+  for (int j = 2; j < n; j++) {
+    if (a[j] - a[i] <= 3) {
+      sdem += tohop(j - i + 1);
+      // cout<<a[i]<<" "<<a[j]<<'\n';
+    } else {
       i++;
       j--;
     }
-    //cout<<j<<"\n";
-    if (j==n-1)
-    {
-      for (int k=i;k<=j-2;k++)
-      {
-        if (a[j]-a[k]<=3) {sdem+=tohop(j-k+1);}
+    // cout<<j<<"\n";
+    if (j == n - 1) {
+      for (int k = i; k <= j - 2; k++) {
+        if (a[j] - a[k] <= 3) {
+          sdem += tohop(j - k + 1);
+        }
       }
     }
   }
-  cout<<sdem;
+  cout << sdem;
   return 0;
-}  
+}
