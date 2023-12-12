7a64834b8a7d46116e66ee21a8e2865c18d44da3
khanh <khanhdacodau2007@gmail.com>
2023-12-12 12:33:50 +0700

    DOAN CON CHIA HET K

--- Pesonal/test.cpp
+++ Pesonal/test.cpp
@@ -2,19 +2,6 @@
 #include <unordered_map>
 
 int main() {
-  std::unordered_map<int, int> myMap;
-
-  // Truy cập khóa không tồn tại, sẽ tạo ra khóa với giá trị 0
-  std::cout << myMap[10] << std::endl; // In ra 0
-
-  // Thêm hoặc cập nhật phần tử
-  myMap[1] = 2;
-  myMap[2] = 3;
-
-  // Truy cập và in ra giá trị
-  for (const auto &i : myMap) {
-    std::cout << "Key: " << i.first << ", Value: " << i.second << std::endl;
-  }
-
+  std::cout << 0 % 4;
   return 0;
 }
