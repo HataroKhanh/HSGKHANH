3fbd4a96376c752e190f808ce6d12bcf3b785bea
BuiDuyKhanh <khanhdacodau2007@gmail.com>
2023-12-13 15:32:26 +0700

    SO HOC 1

---
+++ BAITAPSOHOC/TEST.cpp
@@ -0,0 +1,20 @@
+#include <boost/multiprecision/cpp_int.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace boost::multiprecision;
+
+int main() {
+  cpp_int n;
+  cin >> n;
+
+  cpp_int factorial = 1;
+
+  for (cpp_int i = 1; i <= n; ++i) {
+    factorial *= i;
+  }
+
+  cout << factorial << endl;
+
+  return 0;
+}
--- BAITAPSOHOC/analyse.cpp
+++ BAITAPSOHOC/analyse.cpp
@@ -2,52 +2,41 @@
 #include <vector>
 #define ll long long
 using namespace std;
-ll fa(ll &n) {
-  ll k = 1;
-  for (ll i = 1; i <= n; i++) {
-    k *= i;
-  }
-  return k;
-}
-vector<ll> sannguyento() {
-  vector<ll> san(10e6, 1);
-  san[0] = san[1] = 0;
 
-  for (ll i = 2; i < 1e6; i++) {
-    if (san[i] == 1) {
-      for (ll j = i * i; j < 1e6; j += i) {
-        san[j] = 0;
+ll songuyento(ll &n) {
+  if (n <= 1)
+    return false;
+  for (ll i = 3; i * i < n; i++) {
+    if (n % i == 0)
+      return false;
+  }
+  return true;
+}
+map<int, int> khanh(ll &n) {
+  map<int, int> k;
+  for (ll i = 2; i <= n; i++) {
+    ll j = i, d = 2;
+    while (j != 1) {
+      if (songuyento(d) && j % d == 0) {
+        k[d]++;
+        j /= d;
+      } else {
+        d++;
       }
     }
   }
-  return san;
+  return k;
 }
-void khanh(ll &n, vector<ll> san) {
-  vector<ll> k;
-  ll dem = 0, i = 0;
-  while (1) {
-    while (san[i] != 1) {
-      i++;
-    }
-    if (n % i == 0) {
-      dem++;
-      n /= i;
-    } else {
-      i++;
-      cout << dem << ' ';
-      dem = 0;
-    }
-    if (n == 1)
-      break;
-  }
-  cout << 1 << '\n';
-}
+
 int main(int argc, char *argv[]) {
   ll n;
-  vector<ll> san = sannguyento();
+
   while (cin >> n) {
-    ll k = fa(n);
-    khanh(k, san);
+    map<int, int> k = khanh(n);
+    for (auto &i : k) {
+      cout << i.second << " ";
+    }
+    cout << '\n';
   }
   return 0;
 }
