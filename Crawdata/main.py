0554485c7e4da55459c8e87c142f19736965ac2b
khanh <khanhdacodau2007@gmail.com>
2023-12-31 15:22:53 +0700

    Craw DATA

---
+++ Crawdata/main.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+urlname = "https://www.1sttheworld.com/collections/scottish-tartan-hoodie"
+res = requests.get(urlname)
+
+links = []
+for i in range(2,1360):
+    url = f"https://www.1sttheworld.com/collections/scottish-tartan-hoodie?page={i}"
+    res = requests.get(url)
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.content, "html.parser")
+        divs = soup.find_all('div', class_="row mt16 product-grid")
+        for div in divs:
+            a_tags = div.find_all('a', href=True)
+            for a in a_tags:
+                links.append(a['href'])
+
+        for link in links:
+            print(link)
+    else:
+        print("Failed to retrieve the page")
+with open("text.txt",'w') as f:
+    for i in links:
+        f.writelines(i)
