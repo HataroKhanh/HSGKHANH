9863b4f49bbb01b4c90e86540ff6b081945a4b3f
BuiKhanh2007 <147997236+HataroKhanh@users.noreply.github.com>
2024-02-15 17:31:46 +0700

    Update WOOD.py

--- DETHI/DeThiNamDinh/ChonHSG/2023-2024/WOOD.py
+++ DETHI/DeThiNamDinh/ChonHSG/2023-2024/WOOD.py
@@ -7,11 +7,9 @@ b = [0]
 
 for i in range(1,n+1):
 	b.append(a[i]+b[i-1])
-
-for i in range(1,n+1):
-	db = bisect_right(b[1:],k)
-	if db<=n:
-		if b[db]-b[i-1]==k:
-			dmin = min(dmin,db - i)
-			print(dmin)
-print(db)
+print(b)
+for i in range(n+1):
+    db = bisect_right(b,k+a[i])
+    if db-1<n+1 and b[db]-b[i-1]==k:
+        dmin = min(dmin,db - i+1)
+print(dmin)
