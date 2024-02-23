22ce04cadaf74f0cddb6bb9b871ca3682f076436
BuiKhanh2007 <khanhdacodau2007@gmail.com>
2024-02-24 01:19:36 +0700

    CHIA k

--- DETHI/BACGIANG/2223/CHIAK.py
+++ DETHI/BACGIANG/2223/CHIAK.py
@@ -1,1 +1,20 @@
-n,k = int(input().split())
+from collections import defaultdict
+
+n, k = map(int, input().split())
+f = defaultdict(lambda: 0)
+a = list(map(int, input().split()))
+s = 0
+d = 0
+
+for i in range(n):
+
+    s += a[i]
+
+    if s % k == 0:
+        d += 1
+    if (s - k)%k in f:
+        d += f[(s-k)%k]
+    f[s%k] += 1
+print(d)
+
+
