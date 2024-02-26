83e9e4e484d1976e40dc66bde2e84e86bbde021d
BuiKhanh2007 <khanhdacodau2007@gmail.com>
2024-02-26 09:42:46 +0700

    TEST

--- COBAN/timkiemnhiphan/test.py
+++ COBAN/timkiemnhiphan/test.py
@@ -1,2 +1,18 @@
-f = {1:[123,123]}
-print(f.values())
+def khanh(a,n,k,m):
+    d = 0
+    s = 0
+    v = m*2
+    check = True
+    for i in a:
+        if check:
+            v = i+m*2
+            check = False
+        if i==v:
+            d+=1
+            check = True
+        elif i>v:
+            i-=1
+            d+=1
+            check = True
+    print(d)
+print(khanh([1,2,3,4,5],5,2,3))
